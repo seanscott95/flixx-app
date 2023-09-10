398e942801360b0528eb50c2c20b6a469274f571
Sean <seanms418@gmail.com>
2023-09-10 21:25:30 +1000

    Created the display movie details function that displays each movies details on the movie details html when a movie card is clicked on. Card will show the photo, title, vote, overview, genres, budget, revenue, runtime and production companies.

--- js/script.js
+++ js/script.js
@@ -90,22 +90,90 @@ const displayPopularShows = async () => {
   });
 };
 
+const displayMovieDetails = async () => {
+  const movieID = window.location.search.split('=')[1];
+  console.log("mID", movieID)
+  const movie = await fetchAPIData(`movie/${movieID}`);
+
+  const div = document.createElement('div');
+    
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+        ${
+          movie.poster_path
+            ? `<img
+          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+        : `<img
+          src="../images/no-image.jpg"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+          ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+          movie.budget
+        )}</li>
+        <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+          movie.revenue
+        )}</li>
+        <li><span class="text-secondary">Runtime:</span> ${
+          movie.runtime
+        } minutes</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+        ${movie.production_companies
+          .map((company) => `<span>${company.name}</span>`)
+          .join(', ')}
+      </div>
+    </div>
+  `;
+
+  document.querySelector('#movie-details').append(div);
+};
+
+// Utility Functions
 const showSpinner = () => {
   document.querySelector('.spinner').classList.add('show');
 };
-
 const hideSpinner = () => {
   document.querySelector('.spinner').classList.remove('show');
 };
-
 const highlightActiveLink = () => {
   const links = document.querySelectorAll('.nav-link');
   links.forEach((link) => {
     if (link.getAttribute('href') === global.currentPage) {
       link.classList.add('active');
-    };
+    }
   });
 };
+const addCommasToNumber = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 // Starts app
 const init = () => {
@@ -117,8 +185,8 @@ const init = () => {
     case '/shows.html':
       displayPopularShows();
       break;
-    case '/movies.details':
-      console.log('Movies html');
+    case '/movie-details.html':
+      displayMovieDetails();
       break;
     case '/tv.details':
       console.log('Tv');
@@ -126,9 +194,10 @@ const init = () => {
     case '/search.html':
       console.log('Search');
       break;
-  };
+  }
 
   highlightActiveLink();
 };
 
 document.addEventListener('DOMContentLoaded', init);
+console.log('cp', global.currentPage)
