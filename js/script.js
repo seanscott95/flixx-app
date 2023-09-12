85800be2742c5e2b9d4a9c6a48cc4c972967ec42
Sean <seanms418@gmail.com>
2023-09-12 19:20:27 +1000

    Created the display show details function that displays the show details when the user clicks on a tv show card. The user is taken to the show-details html page that now displays the tv show details that include the background image, title, first air date, overview, genres, number of episodes, last episode to air, and the production companies. The movie details are from the tmdb api. The page also has a button the leads back to the shows page.

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ const displayMovieDetails = async () => {
   const movie = await fetchAPIData(`movie/${movieID}`);
 
   // Overlay for background image
-  displayBackgroundImage('movie', movie.backdrop_path)
+  displayBackgroundImage('movie', movie.backdrop_path);
 
   const div = document.createElement('div');
   div.innerHTML = `
@@ -157,6 +157,67 @@ const displayMovieDetails = async () => {
   document.querySelector('#movie-details').append(div);
 };
 
+const displayShowDetails = async () => {
+  const showID = window.location.search.split('=')[1];
+  const show = await fetchAPIData(`tv/${showID}`);
+  console.log('show', show)
+
+  // Overlay for background image
+  displayBackgroundImage('show', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+        ${
+          show.poster_path
+            ? `<img
+          src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+        : `<img
+          src="../images/no-image.jpg"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+      }
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p class="text-muted">First Aired: ${show.first_air_date}</p>
+        <p>
+          ${show.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number of Episodes:</span> ${addCommasToNumber(
+          show.number_of_episodes
+        )}</li>
+        <li><span class="text-secondary">Last Episode to Air:</span> ${addCommasToNumber(
+          show.last_episode_to_air.name
+        )}</li>
+        <li><span class="text-secondary">Status:</span> ${show.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+        ${show.production_companies
+          .map((company) => `<span>${company.name}</span>`)
+          .join(', ')}
+      </div>
+    </div>
+  `;
+
+  document.querySelector('#show-details').append(div);
+};
+
 const displayBackgroundImage = (type, backgroundPath) => {
   const overlayDiv = document.createElement('div');
   overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
@@ -190,12 +251,12 @@ const highlightActiveLink = () => {
   links.forEach((link) => {
     if (link.getAttribute('href') === global.currentPage) {
       link.classList.add('active');
-    }
+    };
   });
 };
 const addCommasToNumber = (number) => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+};
 
 // Starts app
 const init = () => {
@@ -211,15 +272,15 @@ const init = () => {
       displayMovieDetails();
       break;
     case '/show-details.html':
-      console.log('Tv');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
       break;
-  }
+  };
 
   highlightActiveLink();
 };
 
 document.addEventListener('DOMContentLoaded', init);
-console.log('cp', global.currentPage)
+console.log('cp', global.currentPage);
